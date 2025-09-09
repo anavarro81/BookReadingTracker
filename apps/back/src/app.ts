6454900d5d69cea9a8b2667715144edfbe55f01d
Antonio Navarro <dojeDulDev@Proton.me>
2025-09-09 15:16:13 +0200

    incluide connection in app.ts

--- apps/back/src/app.ts
+++ apps/back/src/app.ts
@@ -3,13 +3,15 @@ import cors from 'cors'
 import {errorHandler} from "./middleware/error.middleware"
 import helmet from 'helmet'
 import bookRouter from './routes/book.routes'
+import connectDB from './config/bd'
 
 const app = express()
 app.use(cors()) 
 app.use(express.json())
 app.use(helmet())
 
-console.log('Estoy en mi app')
+console.log('Conectar a la base de datos')
+connectDB()
 
 app.get('/health', (req, res) => {
     res.status(200).json({status: "OK"})
